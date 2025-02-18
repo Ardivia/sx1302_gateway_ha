6ed843f6161919b065d8dedd37a07393ff6454ae
Ardivia <andrea.negro@ardivia.it>
2025-02-18 14:57:29 +0100

    Update run.sh
    Added gateway_id

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION="1.0.0"
+VERSION="1.0.1"
 
 echo "Running SX1302 LoRa Gateway version: $VERSION"
 
@@ -8,11 +8,13 @@ echo "Running SX1302 LoRa Gateway version: $VERSION"
 CONFIG_PATH=/data/options.json
 echo "Reading configuration from $CONFIG_PATH"
 REGION=$(jq --raw-output '.region' $CONFIG_PATH)
+GATEWAY_ID=$(jq --raw-output '.gateway_id' $CONFIG_PATH)
 IP=$(jq --raw-output '.server_address' $CONFIG_PATH)
 PORT=$(jq --raw-output '.port' $CONFIG_PATH)
 
 echo "Configuration values:"
 echo "REGION: $REGION"
+echo "GATEWAY_ID: $GATEWAY_ID"
 echo "IP: $IP"
 echo "PORT: $PORT"
 
@@ -26,10 +28,10 @@ if [ -f "$CONFIG_FILE" ]; then
     echo "Updating the configuration file with IP: $IP and PORT: $PORT..."
 
     # Update the configuration file with the provided IP and port using sed
+	sed -i "s/\"gateway_ID\": \".*\"/\"gateway_ID\": \"$GATEWAY_ID\"/" "$CONFIG_FILE"
     sed -i "s/\"server_address\": \".*\"/\"server_address\": \"$IP\"/" "$CONFIG_FILE"
     sed -i "s/\"serv_port_up\": [0-9]*/\"serv_port_up\": $PORT/" "$CONFIG_FILE"
     sed -i "s/\"serv_port_down\": [0-9]*/\"serv_port_down\": $PORT/" "$CONFIG_FILE"
-    sed -i "s/\"gps_tty_path\": \".*\"/\"gps_tty_path\": \"\/dev\/ttyS0\"/" "$CONFIG_FILE"
     
 else
     echo "Configuration file $CONFIG_FILE not found!"
