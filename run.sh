2fcbec25207273ac09f534747b32cb38e51aa8e1
Ardivia <andrea.negro@ardivia.it>
2025-03-18 14:01:00 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION="1.1.5"
+VERSION="1.1.6"
 
 echo "Running SX1302 LoRa Gateway version: $VERSION"
 
@@ -11,12 +11,14 @@ REGION=$(jq --raw-output '.region' $CONFIG_PATH)
 GATEWAY_ID=$(jq --raw-output '.gateway_id' $CONFIG_PATH)
 IP=$(jq --raw-output '.server_address' $CONFIG_PATH)
 PORT=$(jq --raw-output '.port' $CONFIG_PATH)
+PORT=$(jq --raw-output '.gps' $CONFIG_PATH)
 
 echo "Configuration values:"
 echo "REGION: $REGION"
 echo "GATEWAY_ID: $GATEWAY_ID"
 echo "IP: $IP"
 echo "PORT: $PORT"
+echo "GPS: $GPS"
 
 CONFIG_FILE="/sx1302_hal/packet_forwarder/global_conf.json.sx1250.${REGION}"
 
@@ -32,7 +34,7 @@ if [ -f "$CONFIG_FILE" ]; then
     sed -i "s/\"server_address\": \".*\"/\"server_address\": \"$IP\"/" "$CONFIG_FILE"
     sed -i "s/\"serv_port_up\": [0-9]*/\"serv_port_up\": $PORT/" "$CONFIG_FILE"
     sed -i "s/\"serv_port_down\": [0-9]*/\"serv_port_down\": $PORT/" "$CONFIG_FILE"
-    sed -i "s/\"gps_tty_path\": \".*\"/\"gps_tty_path\": \"\/dev\/ttyAMA0\"/" "$CONFIG_FILE"
+    sed -i "s/\"gps_tty_path\": \".*\"/\"gps_tty_path\": $GPS/" "$CONFIG_FILE"
 else
     echo "Configuration file $CONFIG_FILE not found!"
     exit 1
